7934249b61a8798a7ca1a7750f4acd30769fd233
cge85 <cg_emers@live.nl>
2022-08-08 22:04:36 +0200

    Day 10 Done

---
+++ PYTHON/100  Days Of Code by Dr. Angela Yu/Leap_Year_with_return_statement.py
@@ -0,0 +1,29 @@
+def is_leap(year):
+    #This is how you create a Docstring for your function.
+    '''Will define if leap year when entering the year'''
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+
+def days_in_month(year, month):
+    #This is how you create a Docstring for your function.
+    '''Will show how many days in a month when providing the year and month'''
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap(year) and  month == 2:
+        return 29
+    return month_days[month - 1]
+
+
+# 🚨 Do NOT change any of the code below
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
