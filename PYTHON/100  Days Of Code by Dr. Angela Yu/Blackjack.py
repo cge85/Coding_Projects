e1b67ad3798ea90a39923f6e561f601ea2d21c28
cge85 <cg_emers@live.nl>
2022-08-11 21:48:11 +0200

    updated blackjack project

--- PYTHON/100  Days Of Code by Dr. Angela Yu/Blackjack.py
+++ PYTHON/100  Days Of Code by Dr. Angela Yu/Blackjack.py
@@ -6,26 +6,53 @@ import os
 clear = lambda: os.system('cls')
 
 
-play = input("Do You want to play a game of Blackjack? Type 'y' or 'n': ")
 
-print(logo)
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-def user_draw():
+def play_blackjack():
+    print(logo)
+    print(input("Do You want to play a game of Blackjack? Type 'y' or 'n': "))
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+
+
+
+    add_card = True
+    
     for card in cards:
         user_card1 = randint(1, card)
         user_card2 = randint(1, card)
-        user_cards = [user_card1, user_card2]
-        score = user_card1 + user_card2
-    print(f"Your cards: {user_cards}, current score: {score}")
-user_draw()
+        user_card_list = [user_card1, user_card2]
+        user_score = user_card1 + user_card2
+        computer_card = randint(1, card)
+        computer_card_list = [computer_card]
+        computer_score = computer_card
+    print(f"Your cards: {user_card_list}, current score: {user_score}")
+    print(f"Computer's first card: {computer_card}")
+    
+    while add_card:
+        another_card = input("Type 'y' to get another card, type 'n' to pass: ")
+        if  another_card == "y" and user_score < 21:
+                user_card = randint(1, card)
+                user_card_list += [user_card]
+                user_score += user_card
+                print(f"Your cards: {user_card_list}, current score: {user_score}")
+        else:
+            print(f"Your final hand: {user_card_list}, final score: {user_score}")
+            print(f"Computer's final hand: {computer_card_list}, final score: {computer_score}")
+            print("You went over. You lose 😭")
+            add_card = False
 
-def computer_draw():
-    for card in cards:
-        computer_card1 = randint(1, card)
-    print(f"Computer's first card: {computer_card1}")
-computer_draw()
-
-if input("Type 'y' to get another card, type 'n' to pass: ") == "y":
-    user_card3 = 
+        if another_card == "n":
+            computer_card_list += computer_card
+            computer_score += computer_card_list[computer_card]
+            if computer_score < 21 and computer_score > user_score:
+                print(f"Your final hand: {user_card_list}, final score: {user_score}")
+                print(f"Computer's final hand: {computer_card_list}, final score: {computer_score}")
+                print("Computer Win")
+        # else:
+        #     print(f"Your final hand: {user_card_list}, final score: {user_score}")
+        #     print(f"Computer's final hand: {computer_card_list}, final score: {computer_score}")
+        #     print("You went over. You lose 😭")
+        #     add_card = False
+play_blackjack()
