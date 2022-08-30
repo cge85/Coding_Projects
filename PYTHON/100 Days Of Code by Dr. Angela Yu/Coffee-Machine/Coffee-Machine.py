91ef41b1cd6a24e769751dea7e4dd2c5cb990f7b
cge85 <cg_emers@live.nl>
2022-08-30 22:10:21 +0200

    coffee machine completed need to learn functions

--- PYTHON/100 Days Of Code by Dr. Angela Yu/Coffee-Machine/Coffee-Machine.py
+++ PYTHON/100 Days Of Code by Dr. Angela Yu/Coffee-Machine/Coffee-Machine.py
@@ -1,8 +1,9 @@
 from data import MENU
 from Logo import logo
 
+
 print(logo)
-power_on = True
+
 
 es_water = MENU["espresso"]["ingredients"]['water']
 es_coffee = MENU["espresso"]["ingredients"]['coffee']
@@ -19,23 +20,29 @@ ca_coffee = MENU["cappuccino"]["ingredients"]['coffee']
 ca_milk = MENU["cappuccino"]["ingredients"]['milk']
 ca_cost = MENU["cappuccino"]["cost"]
 
+water = 300
+milk = 200
+coffee = 100
+money = 0
 
-user_insert_coins = 0
-change = 0
-
+power_on = True
 
 
 while power_on:
-    water = 300
-    milk = 200
-    coffee = 100
-    money = 0
-    print_report = f"Water: {water}ml\nMilk: {milk}ml\nCoffee: {coffee}g\nMoney: ${money}"
+    user_insert_coins = 0
+    change = 0
 
     user_choice = input("What would you like? (espresso/latte/cappuccino): ")
-    
 
     if user_choice == "espresso":
+        if water >= es_water and coffee >= es_coffee:
+            water -= es_water
+            coffee -= es_coffee
+            money += es_cost
+        else:
+            print("Sorry not enough water.")
+            continue
+            
         print("Please insert coins.")
         quaters = float(input("how many quaters?: "))
         user_insert_coins += quaters * 0.25
@@ -46,17 +53,23 @@ while power_on:
         pennies = float(input("how many pennies?: "))
         user_insert_coins += pennies * 0.01
         if user_insert_coins > es_cost:
-            if es_water < water and es_coffee < coffee:
-                water - es_water
-                coffee - es_coffee
-                money + es_cost
             change = float(round(user_insert_coins - es_cost, 2))
             print(f"Here is ${change} in change.")
             print("Here is your espresso ☕. Enjoy!")
+            
         else:
             print("Sorry that's not enough money. Money refunded.")
 
     elif user_choice == "latte":
+        if water >= la_water and coffee >= la_coffee and milk >= la_milk:
+            water -= la_water
+            coffee -= la_coffee
+            milk -= la_milk
+            money += la_cost
+        else:
+            print("Sorry not enough water.")
+            continue
+
         print("Please insert coins.")
         quaters = float(input("how many quaters?: "))
         user_insert_coins += quaters * 0.25
@@ -67,18 +80,23 @@ while power_on:
         pennies = float(input("how many pennies?: "))
         user_insert_coins += pennies * 0.01
         if user_insert_coins > la_cost:
-            if la_water < water and la_coffee < coffee and la_milk < milk:
-                water -= la_water
-                coffee -= la_coffee
-                milk -= la_milk
-                money += la_cost
             change = float(round(user_insert_coins - la_cost, 2))
             print(f"Here is ${change} in change.")
             print("Here is your latte ☕. Enjoy!")
+            
         else:
             print("Sorry that's not enough money. Money refunded.")
 
     elif user_choice == "cappuccino":
+        if water >= ca_water and coffee >= ca_coffee and milk >= ca_milk:
+            water -= ca_water
+            coffee -= ca_coffee
+            milk -= ca_milk
+            money += ca_cost
+        else:
+            print("Sorry not enough water.")
+            continue
+        
         print("Please insert coins.")
         quaters = float(input("how many quaters?: "))
         user_insert_coins += quaters * 0.25
@@ -89,14 +107,9 @@ while power_on:
         pennies = float(input("how many pennies?: "))
         user_insert_coins += pennies * 0.01
         if user_insert_coins > ca_cost:
-            if ca_water < water and ca_coffee < coffee and ca_milk < milk:
-                water -= ca_water
-                coffee -= ca_coffee
-                milk -= ca_milk
-                money += ca_cost
             change = float(round(user_insert_coins - ca_cost, 2))
             print(f"Here is ${change} in change.")  
-            print("Here is your cappuccino ☕. Enjoy!")
+            print("Here is your cappuccino ☕. Enjoy!")         
         else:
             print("Sorry that's not enough money. Money refunded.")
     
@@ -106,3 +119,4 @@ while power_on:
     elif user_choice == "off":
         power_on = False
 
+    print_report = f"Water: {water}ml\nMilk: {milk}ml\nCoffee: {coffee}g\nMoney: ${money}"
--- PYTHON/100 Days Of Code by Dr. Angela Yu/Coffee-Machine/tempCodeRunnerFile.py
+++ PYTHON/100 Days Of Code by Dr. Angela Yu/Coffee-Machine/tempCodeRunnerFile.py
@@ -1,1 +1,9 @@
-i
+    print("Please insert coins.")
+            quaters = float(input("how many quaters?: "))
+            user_insert_coins += quaters * 0.25
+            dimes = float(input("how many dimes?: "))
+            user_insert_coins += dimes * 0.10
+            nickles = float(input("how many nickles?: "))
+            user_insert_coins += nickles * 0.05
+            pennies = float(input("how many pennies?: "))
+            user_i
