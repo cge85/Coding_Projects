3af97ec74bcb4b7e54c242139a3475a0cce7b4ff
cge85 <cg_emers@live.nl>
2022-09-12 21:30:56 +0200

    Day 18

--- PYTHON/100 Days Of Code by Dr. Angela Yu/quiz-game-start/quiz_brain.py
+++ PYTHON/100 Days Of Code by Dr. Angela Yu/quiz-game-start/quiz_brain.py
@@ -1,11 +1,13 @@
+from distutils.ccompiler import gen_lib_options
+
+
 class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
-        self.score = 0
         self.question_list = q_list
-        
+        self.score = 0
 
-    def still_has_questions(self):
+    def still_have_question(self):
         return self.question_number < len(self.question_list)
 
     def next_question(self):
@@ -16,8 +18,8 @@ class QuizBrain:
 
     def check_answer(self, user_answer, correct_answer):
         if user_answer.lower() == correct_answer.lower():
-            self.score += 1
             print("You got it right!")
+            self.score += 1
         else:
             print("That's wrong.")
         print(f"The correct answer was: {correct_answer}")
