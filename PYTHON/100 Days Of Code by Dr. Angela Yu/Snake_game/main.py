d9542ad5df6d33fd41d2eaed90166de2b2b87938
cge85 <cg_emers@live.nl>
2022-09-21 07:49:15 +0200

    Snake Game

--- PYTHON/100 Days Of Code by Dr. Angela Yu/Snake_game/main.py
+++ PYTHON/100 Days Of Code by Dr. Angela Yu/Snake_game/main.py
@@ -14,11 +14,11 @@ starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 segments = []
 
 for position in starting_positions:
-    segment = Turtle("square")
-    segment.color("white")
-    segment.penup() 
-    segment.goto(position)
-    segments.append(segment)
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.penup() 
+    new_segment.goto(position)
+    segments.append(new_segment)
 
 
 
