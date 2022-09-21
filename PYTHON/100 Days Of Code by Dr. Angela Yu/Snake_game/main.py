8e99acf1157b3a0b836cd55af502d553ec743086
cge85 <cg_emers@live.nl>
2022-09-21 07:47:19 +0200

    Snake Game

--- PYTHON/100 Days Of Code by Dr. Angela Yu/Snake_game/main.py
+++ PYTHON/100 Days Of Code by Dr. Angela Yu/Snake_game/main.py
@@ -11,14 +11,14 @@ screen.tracer(0)
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-snakes = []
+segments = []
 
 for position in starting_positions:
-    snake = Turtle("square")
-    snake.color("white")
-    snake.penup() 
-    snake.goto(position)
-    snakes.append(snake)
+    segment = Turtle("square")
+    segment.color("white")
+    segment.penup() 
+    segment.goto(position)
+    segments.append(segment)
 
 
 
@@ -28,11 +28,11 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     
-    for snake_number in range(len(snakes) -1, 0, -1):
-        new_x = snakes[snake_number - 1].xcor()
-        new_y = snakes[snake_number - 1].ycor()
-        snakes[snake_number].goto(new_x, new_y)
+    for seg_number in range(len(segments) -1, 0, -1):
+        new_x = segments[seg_number - 1].xcor()
+        new_y = segments[seg_number - 1].ycor()
+        segments[seg_number].goto(new_x, new_y)
         
-        
+    segments[0].forward(20)
 
 screen.exitonclick()
